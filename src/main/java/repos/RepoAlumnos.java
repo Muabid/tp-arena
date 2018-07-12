e7217ddb1dd36254a38254849a921b454974a2a3
KAREN <lafuentekaren@gmail.com>
2018-07-12 03:28:20 -0300

    Método para obtener contraseñas

--- src/main/java/repos/RepoAlumnos.java
+++ src/main/java/repos/RepoAlumnos.java
@@ -26,14 +26,18 @@ public class RepoAlumnos {
 		return alumnos.stream().filter(a->a.getLegajo().equals(legajo)).findFirst().get();
 	}
 	
+	public static String obtenerContraseña(String legajo) {
+		return alumnos.stream().filter(a->a.getLegajo().equals(legajo)).findFirst().get().getContraseña();
+	}
+	
 	
 	
 	public static void start() {
 		Asignacion<Numerica> asig = new Asignacion<>("TP Arena",null, Arrays.asList(new Numerica(10),new Numerica(5)));
 		Asignacion<Conceptual> asig2= new Asignacion<>("Parcial",null,Arrays.asList(new Conceptual('M',null)));
 		
-		Alumno bruno= new Alumno("Bruno", "Cobos", "1223", "Muabid",Arrays.asList(asig,asig2));
-		Alumno karen= new Alumno("Karen","Lafuente","1314","Kalafuente",Arrays.asList(asig));
+		Alumno bruno= new Alumno("Bruno", "Cobos", "1223", "Muabid","holis",Arrays.asList(asig,asig2));
+		Alumno karen= new Alumno("Karen","Lafuente","1314","Kalafuente","diosa",Arrays.asList(asig));
 		alumnos.add(bruno);
 		alumnos.add(karen);
 				
