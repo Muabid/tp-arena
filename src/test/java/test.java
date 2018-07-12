a4334f3f546ccb4a1e189f964ed107235c101d47
Bruno <bruno2698@gmail.com>
2018-07-12 14:48:47 -0300

    Agrego junit y el init del test

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -1,4 +1,24 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import domain.Alumno;
+import junit.framework.Assert;
+import repos.RepoAlumnos;
+
 
 public class test {
-
+	Alumno bruno;
+	Alumno karen;
+	
+	@Before
+	public void init() {
+		
+		RepoAlumnos.start();
+		bruno=RepoAlumnos.buscar("1223");
+		karen=RepoAlumnos.buscar("1314");
+		new App().start();	
+	}
+	
+	
+	
 }
